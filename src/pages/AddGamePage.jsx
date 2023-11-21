ad13952b315d9195214e25aba1f3385b8c2acbe7
Vasco Martins <vasco.martins.drummer@gmail.com>
2023-11-21 15:20:58 +0000

    added semantic ui dropdown

--- src/pages/AddGamePage.jsx
+++ src/pages/AddGamePage.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
+import { Dropdown, FormDropdown } from 'semantic-ui-react'
 import axios from 'axios'
 
 const API_URL = "http://localhost:5005"
@@ -14,7 +15,66 @@ function AddGamePage() {
     const [platform, setPlatform] = useState("")
     const [imageUrl, setImageUrl] = useState("")
 
+    const [genresList, setGenresList] = useState(null)
+    const [platformsList, setPlatformsList] = useState(null)
+
     const navigate = useNavigate()
+
+    useEffect(() => {
+        axios
+            .get(API_URL + "/games")
+            .then((response) => {
+                setGenresList(getGenres(response.data))
+                setPlatformsList(getPlatforms(response.data))
+            })
+            .catch((error) => {
+                console.log("An error occurred: ");
+                console.log(error);
+            });
+    }, []);
+
+    function getGenres(data) {
+
+        const allGenres = data.map((elm) => elm.genre)
+        const temp = looper(allGenres);
+        const genres = sortObject(temp)
+        return genres
+    }
+
+    function getPlatforms(data) {
+
+        const allPlatforms = data.map((elm) => elm.platform)
+        let temp = looper(allPlatforms);
+        const platforms = sortObject(temp);
+        return platforms
+    }
+
+    // Function to sort Genre and Platform alphabetically
+    function sortObject(data) {
+        return data.sort((a, b) => {
+            return a.text.localeCompare(b.text)
+        })
+    }
+
+    function looper(type) {
+
+        const newArray = []
+
+        for (let i = 0; i < type.length; i++) {
+            for (let j = 0; j < type[i].length; j++) {
+                const exists = newArray.some(elm => elm.key === type[i][j])
+                if (!exists) {
+                    newArray.push({
+                        key: type[i][j],
+                        text: type[i][j],
+                        value: type[i][j]
+                    })
+                }
+            }
+        }
+
+        return newArray
+    }
 
     const handleSubmit = ((e) => {
         e.preventDefault()
@@ -24,8 +84,8 @@ function AddGamePage() {
             description: description,
             rating: parseFloat(rating),
             price: parseFloat(price),
-            genre: SplitString(genre),
-            platform: SplitString(platform),
+            genre: genre,
+            platform: platform,//SplitString(platform),
             image_url: imageUrl,
             free: (price <= 0 ? true : false)
         }
@@ -41,14 +101,22 @@ function AddGamePage() {
 
     })
 
-    function SplitString(inputString) {
+    const handlePlatformSelection = (event, data) => {
+        setPlatform(data.value)
+    }
+
+    const handleGenreSelection = (event, data) => {
+        setGenre(data.value)
+    }
+
+    /* function SplitString(inputString) {
 
         if (inputString.length === 0) return [];
 
         const outputArray = inputString.split(',')
 
         return outputArray;
-    }
+    } */
 
     return (
         <div className="FormDiv">
@@ -98,7 +166,7 @@ function AddGamePage() {
                         onChange={(e) => { setPrice(e.target.value) }}
                     />
                 </label>
-                <label>
+                {/* <label>
                     <p>Genre(s)</p>
                     <input
                         type="text"
@@ -108,8 +176,32 @@ function AddGamePage() {
                         placeholder="Separate genres with a comma"
                         onChange={(e) => { setGenre(e.target.value) }}
                     />
+                </label> */}
+                <label>
+                    Genres
+                    <Dropdown
+                        placeholder='GENRES'
+                        fluid={false}
+                        multiple
+                        selection
+                        onChange={handleGenreSelection}
+                        options={genresList}
+                    />
                 </label>
                 <label>
+                    Platforms
+                    <Dropdown
+                        placeholder='PLATFORMS'
+                        fluid={false}
+                        multiple
+                        selection
+                        onChange={handlePlatformSelection}
+                        options={platformsList}
+                    />
+                </label>
+
+
+                {/* <label>
                     <p>Platform(s)</p>
                     <input
                         type="text"
@@ -119,7 +211,7 @@ function AddGamePage() {
                         placeholder="Separate platforms with a comma"
                         onChange={(e) => { setPlatform(e.target.value) }}
                     />
-                </label>
+                </label> */}
                 <label>
                     <p>Image URL</p>
                     <input
