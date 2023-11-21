7b7415682db3f4483ae64cd3f20568f25514c812
Vasco Martins <vasco.martins.drummer@gmail.com>
2023-11-21 10:47:44 +0000

    removed filters from recommended games and implemented in browse games

--- src/pages/GamesListPage.jsx
+++ src/pages/GamesListPage.jsx
@@ -6,6 +6,9 @@ const API_URL = "http://localhost:5005";
 
 function GamesListPage() {
   const [gamesList, setGamesList] = useState(null);
+  const [displayList, setDisplayList] = useState(null)
+  const [genresList, setGenresList] = useState(null)
+  const [platformsList, setPlatformsList] = useState(null)
   const [searchGames, setSearchGames] = useState("");
 
 
@@ -14,20 +17,86 @@ function GamesListPage() {
       .get(API_URL + "/games")
       .then((response) => {
         setGamesList(response.data);
+        setDisplayList(response.data)
+        return response
+      })
+      .then((response) => {
+        setGenresList(getGenres(response.data))
+        setPlatformsList(getPlatforms(response.data))
       })
       .catch((error) => {
         console.log("An error occurred: ");
         console.log(error);
       });
   }, []);
+
+  // Search related functions
   const handleSearch = (e) => {
     e.preventDefault();
+
     setSearchGames(e.target.value);
   };
 
-  const games = gamesList === null ? [] : gamesList.filter((game) => {
-    return game.title.toLowerCase().includes(searchGames.toLowerCase());
-  });
+  useEffect(() => {
+
+    const games = searchGames === "" ? gamesList : gamesList.filter((game) => {
+      return game.title.toLowerCase().includes(searchGames.toLowerCase());
+    })
+
+    setDisplayList(games);
+    
+  }, [searchGames])
+
+  // Populate states for filters
+  function getGenres(data) {
+
+    const allGenres = data.map((elm) => elm.genre)
+    const genres = looper(allGenres);
+
+    return genres
+  }
+
+  function getPlatforms(data) {
+
+    const allPlatforms = data.map((elm) => elm.platform)
+    const platforms = looper(allPlatforms);
+    return platforms
+  }
+
+  function looper(type) {
+
+    const newArray = [];
+
+    for (let i = 0; i < type.length; i++) {
+      for (let j = 0; j < type[i].length; j++) {
+        if (newArray.indexOf(type[i][j]) === -1) {
+          newArray.push(type[i][j])
+        }
+      }
+    }
+    return newArray
+  }
+
+  // Handle selected filters
+  const filterPlatformHandler = (event) => {
+    if (event.target.checked) {
+      setDisplayList(gamesList.filter((elm) => {
+        return elm.platform.includes(event.target.value)
+      }))
+    } else {
+      setDisplayList(gamesList)
+    }
+  }
+
+  const filterGenreHandler = (event) => {
+    if (event.target.checked) {
+      setDisplayList(gamesList.filter((elm) => {
+        return elm.genre.includes(event.target.value)
+      }))
+    } else {
+      setDisplayList(gamesList)
+    }
+  }
 
   return (
     <>
@@ -39,12 +108,59 @@ function GamesListPage() {
           onChange={handleSearch}
         />
       </div>
-
+      <div className="FilterOptionsDiv">
+        Genres
+        <div className="GenresDiv">
+          {genresList === null
+            ? <h3>Loading...</h3>
+            : (
+              genresList.map((genre, index) => {
+                return (
+                  <span className="GenresCheckbox" key={index}>
+                    <label>
+                      {genre}
+                      <input
+                        type="checkbox"
+                        onChange={filterGenreHandler}
+                        value={genre}
+                      />
+                    </label>
+                  </span>
+                )
+              })
+            )
+          }
+        </div>
+      </div>
+      <div className="FilterOptionsDiv">
+        Platforms
+        <div className="GenresDiv">
+          {platformsList === null
+            ? <h3>Loading...</h3>
+            : (
+              platformsList.map((platform, index) => {
+                return (
+                  <span className="GenresCheckbox" key={index}>
+                    <label>
+                      {platform}
+                      <input
+                        type="checkbox"
+                        onChange={filterPlatformHandler}
+                        value={platform}
+                      />
+                    </label>
+                  </span>
+                )
+              })
+            )
+          }
+        </div>
+      </div>
       <div className="GamesListDiv">
-        {games === null ? (
+        {displayList === null ? (
           <h1>Game list loading...</h1>
         ) : (
-          games.map((game) => {
+          displayList.map((game) => {
             return (
               <div className="GamesListImageDiv" key={game.id}>
                 <Link to={`/games/${game.id}`}>
